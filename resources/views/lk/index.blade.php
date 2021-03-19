776509693a063833be81a3d5a9bc4f60794a9166
eovchinnikov <agr.akyla@mail.ru>
2021-03-19 11:07:52 +0300

    Добавлен новый дизайн таблицы, который защищает от её расширения за пределы сайта.

--- resources/views/lk/index.blade.php
+++ resources/views/lk/index.blade.php
@@ -4,21 +4,16 @@
     <div class="">
         <h1>Информация о заказе</h1>
 
-        <table class="table table-striped table-hover">
+        <table class="table table-bordered table-checkable dataTable no-footer dtr-inline collapsed">
             <thead>
             <tr>
                 <th scope="col">ПРЖ</th>
                 <th scope="col">Максимальная дата поставки по ПРЖ</th>
                 <th scope="col"> Запрошенная дата поставки по ПРЖ</th>
-{{--                <th scope="col">SALESNAME</th> --}}
-{{--                <th scope="col">CUSTACCOUNT</th>--}}
-{{--                <th scope="col">PRODSTATUS</th>--}}
                 <th scope="col">Количество заказанное клиентом по ПРЖ</th>
                 <th scope="col">Количество по ПРЖ</th>
                 <th scope="col">КПП/ИНН</th>
                 <th scope="col">Менеджер</th>
-{{--                <th scope="col">CREATEDATETIME</th>--}}
-{{--                <th scope="col">UserID</th>--}}
             </tr>
             </thead>
             <tbody>
@@ -27,15 +22,10 @@
                         <th scope="row">{{ $order->PRJ }}</th>
                         <th scope="row">{{ $order->DlvDate }}</th>
                         <th scope="row">{{ $order->ShippingDateRequested }}</th>
-{{--                        <th scope="row">{{ $order->SALESNAME }}</th>--}}
-{{--                        <th scope="row">{{ $order->CUSTACCOUNT }}</th>--}}
-{{--                        <th scope="row">{{ $order->PRODSTATUS }}</th>--}}
                         <th scope="row">{{ $order->QTYORDERED }}</th>
                         <th scope="row">{{ $order->QTYSCHED }}</th>
                         <th scope="row">{{ $order->REGISTRATIONNUMBER }}</th>
                         <th scope="row">{{ $order->Manager }}</th>
-{{--                        <th scope="row">{{ $order->CREATEDATETIME }}</th>--}}
-{{--                        <th scope="row">{{ $order->UserID }}</th>--}}
                     </tr>
                 @endforeach
             </tbody>
