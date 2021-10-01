65748aad1fa1606c4cbafeeafcf731e3c5996a79
eovchinnikov <agr.akyla@mail.ru>
2021-10-01 09:50:30 +0300

    Fix field

--- app/Services/WarehouseService.php
+++ app/Services/WarehouseService.php
@@ -329,8 +329,8 @@ class WarehouseService
                 $msg .= $item->BATCH . PHP_EOL;
                 $msg .= $item->NAMEALIAS . PHP_EOL;
 //                $msg .= $item->COLORID . PHP_EOL;
-                $msg .= $item->WMSLOCATION . PHP_EOL;
-                $msg .= $item->LICENSE . PHP_EOL;
+                $msg .= "Яч: " . $item->WMSLOCATION . PHP_EOL;
+                $msg .= "НЗ: " . $item->LICENSE . PHP_EOL;
                 $msg .= PHP_EOL;
             }
 
