ca0c30a9fc16146673d33d729173958956e4e891
Ckala <712020:e10af0f5-a9cb-41a2-b3fc-13b9fa438c44@bots.bitbucket.org>
2024-01-12 11:19:38 +0300

    (feature) add column is_show_rdp for user table and fix sidebar.blade.php

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -103,7 +103,9 @@
                             <span class="menu-text">Пользователи</span>
                         </a>
                     </li>
+                @endif
 
+                @if($user->is_admin == true || $user->is_show_rdp == true)
                     <li class="menu-item" aria-haspopup="true">
                         <a href="/key-logger-dashboard" class="menu-link ">
                         <span class="svg-icon menu-icon"><!--begin::Svg Icon | path:media/svg/icons/Design/Layers.svg-->
@@ -121,6 +123,9 @@
                             <span class="menu-text">Активность пользователей</span>
                         </a>
                     </li>
+                @endif
+
+                @if($user->is_admin == true)
 
                     <li class="menu-item" aria-haspopup="true">
                         <a href="/windows/server" class="menu-link ">
